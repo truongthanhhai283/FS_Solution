25e3ba0e7e8528326ad481ea5ed231e4d73fb08d
TruongThanhHai28 <truongthanhhai283@gmail.com>
2020-08-15 08:54:47 +0700

    Creating ShopOnline Demo Solution (Index - Cart - Details)

---
+++ FE Trainning/TheWorldPhone/assets/js/main.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+    sliderCarousel();
+    bannerSlider();
+    seeMore();
+    backToTop();
+
+});
+
+function sliderCarousel() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            900: {
+                items: 3
+            },
+            1200: {
+                items: 5
+            }
+        }
+    })
+}
+
+function bannerSlider() {
+    var jssor_1_SlideshowTransitions = [
+        { $Duration: 800, x: -0.3, $During: { $Left: [0.3, 0.7] }, $Easing: { $Left: $Jease$.$InCubic, $Opacity: $Jease$.$Linear }, $Opacity: 2 },
+        { $Duration: 800, x: 0.3, $SlideOut: true, $Easing: { $Left: $Jease$.$InCubic, $Opacity: $Jease$.$Linear }, $Opacity: 2 }
+    ];
+
+    var jssor_1_options = {
+        $AutoPlay: 1,
+        $SlideshowOptions: {
+            $Class: $JssorSlideshowRunner$,
+            $Transitions: jssor_1_SlideshowTransitions,
+            $TransitionsOrder: 1
+        },
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+        },
+        $ThumbnailNavigatorOptions: {
+            $Class: $JssorThumbnailNavigator$,
+            $Orientation: 2,
+            $NoDrag: true
+        }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    /*#region responsive code begin*/
+
+    var MAX_WIDTH = 980;
+
+    function ScaleSlider() {
+        var containerElement = jssor_1_slider.$Elmt.parentNode;
+        var containerWidth = containerElement.clientWidth;
+
+        if (containerWidth) {
+
+            var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+            jssor_1_slider.$ScaleWidth(expectedWidth);
+        } else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+
+    ScaleSlider();
+
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    /*#endregion responsive code end*/
+}
+
+function seeMore() {
+    $('#showSeeMore').click(function() {
+        $('.footer__content-item-see_more-content').show();
+        $('.footer__content-item-link-seemore').hide();
+    });
+}
+
+function backToTop() {
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 100, // px
+            backToTop = function() {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function() {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function(e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
+}
